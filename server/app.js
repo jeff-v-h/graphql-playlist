f604260d861395d21729d3e1c7d00c8376ea6720
jeffrey-huang-recaura <jeffreyhuang@recaura.com>
2021-08-18 21:46:55 +1000

    init initial book schema

--- server/app.js
+++ server/app.js
@@ -5,12 +5,12 @@ const app = express();
 
 // bind express with graphql
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    // pass in a schema property
-  })
+    '/graphql',
+    graphqlHTTP({
+        // pass in a schema property
+    })
 );
 
 app.listen(4000, () => {
-  console.log('now listening for requests on port 4000');
+    console.log('now listening for requests on port 4000');
 });

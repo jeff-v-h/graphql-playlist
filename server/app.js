2869445ed8b9267cca5086bd7c94d4f604c59182
Jeffrey Huang <jeffvh@outlook.com>
2021-08-19 20:49:15 +1000

    add graphiql

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // bind express with graphql
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');

e3061694012a0311140cb5402ad90d86373c2af8
Jeffrey Huang <jeffvh@outlook.com>
2021-08-20 21:19:34 +1000

    remove files not going ot be used and add inital book list component

---
+++ client/src/components/Booklist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BookList extends React.Component {
+  render() {
+    return (
+      <ul className="booklist">
+        <li>book name</li>
+      </ul>
+    );
+  }
+}
+
+export default BookList;

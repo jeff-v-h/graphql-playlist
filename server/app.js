b2c31e16da5ce6a4a9e88d05e502c1a18662f14b
Jeffrey Huang <jeffvh@outlook.com>
2021-08-20 20:09:34 +1000

    add mongoose

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect(
+    'mongodb+srv://jeff:RjXFc4R7VUr4D50w@cluster0.aumx1.mongodb.net/gql-ninja?retryWrites=true&w=majority'
+);
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
 // bind express with graphql
-app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true
+    })
+);
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
